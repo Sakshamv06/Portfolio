1eebb984121f45fb9a5fa3968877482a9c25894a
Saksham <Sakshamv06@gmail.com>
2024-09-06 16:50:52 +0530

    Your commit message

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,96 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Frontend Development : React, Next.js</li>
+        <li>Backend Development : Node.js</li>
+        <li>Databases : MongoDB, Prisma, SQL, PostgreSQL</li>
+        <li>DevOps Tools : Docker, Jenkins, GitLab, Podman, Ansible</li>
+        <li>Version Control : Git, GitHub, GitLab</li>
+        <li>Programming Languages : Java, JavaScript, Python</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>MCA : Vivekananda Institute of Professional Studies (2022-2024)  </li>
+        <li>BCA : Shri Guru Tegh Bahadhur Institute of Management and Information Technology (2019-2022) </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: "Experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Python Developer Intern : AllSoft Solution (Summer Internship 2021)</li>
+        <li>DevOps Intern : Keen and Able Computers Private Limited (April 2024 - Till Present)</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a passionate and motivated software engineer with a background in MCA and a strong interest in DevOps, full-stack development, and automation.
+            I have experience working with technologies like Docker, Jenkins, Next.js, MongoDB, and more.
+            My journey into the tech world has been driven by a deep desire to learn and apply the latest tools and practices in the software industry.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("Experience")}
+              active={tab === "Experience"}
+            >
+              {" "}
+              Experience{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "SamCode",
+    description: "My Portfolio showcasing my skills",
+    image: "/images/projects/p1.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Sakshamv06/Portfolio",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "SaVyaMo",
+    description: "A project Management app for managing data of Developers and Managers using Python Django ",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Sakshamv06/SaVyaMo",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "CWC 2023 Analysis",
+    description: "This project focuses on analyzing the data from the Cricket World Cup 2023 using Python. The aim was to extract meaningful insights and trends from match statistics, player performances, and team strategies throughout the tournament.",
+    image: "/images/projects/3.png",
+    tag: ["All", "Data Analysis"],
+    gitUrl: "https://github.com/Sakshamv06/CWC2023",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "To-Do",
+    description: "A to-do list web application that allows users to sign in/sign up using Clerk. Database management is handled through MongoDB Atlas with Prisma as the ORM.",
+    image: "/images/projects/4.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Sakshamv06/To_Do",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "DevOps CI/CD Pipeline for To-Do App",
+    description: "Developed and automated a CI/CD pipeline using Jenkins and Docker. The pipeline pushes the to-do app code into a Docker server, streamlining the deployment process",
+    image: "/images/projects/5.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Sakshamv06/To_Do",
+    previewUrl: "/",
+  },
+
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Data Analysis"
+          isSelected={tag === "Data Analysis"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
