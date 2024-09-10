3af53db09c90450d54683a66697873209c82601a
Saksham <Sakshamv06@gmail.com>
2024-09-10 22:07:29 +0530

    finalcommit

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#4d4d4e]">
+    <main className="flex min-h-screen flex-col bg-[#050101]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
